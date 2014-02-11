7d964c9e8a10c0a0921ced97fe2bba29c5ba5741
Paul Malyschko <paulm@n0prisoners.com>
2014-02-11 22:01:50 +1030

    Fix scraped URL in install-appengine.sh
    The URL is mentioned twice on the Google App Engine downloads page, as
    the zip file includes both the Python and PHP SDKs.

--- install-appengine.sh
+++ install-appengine.sh
@@ -10,10 +10,10 @@ do_exit() {
 }
 trap do_exit INT EXIT
  
-DL_URL="$(egrep -o 'http[^"]+google_appengine_[.0-9]+\.zip' "$TMPFILE")"
+DL_URL="$(egrep -m 1 -o 'http[^"]+google_appengine_[.0-9]+\.zip' "$TMPFILE")"
 DL_FILE="${DL_URL##*/}"
-DL_SHA1="$(egrep -A2 'google_appengine.*\.zip' "$TMPFILE" | egrep -o '[a-f0-9]{40}')"
-echo "URL : $DL_URL"
+DL_SHA1="$(egrep -A2 'google_appengine.*\.zip' "$TMPFILE" | egrep -m 1 -o '[a-f0-9]{40}')"
+echo "URL: $DL_URL"
 echo "SHA1: $DL_SHA1"
 echo
  
@@ -23,7 +23,7 @@ if [[ ( -z $DL_URL ) || ( -z $DL_FILE ) || ( -z $DL_SHA1 ) ]]; then
 fi
  
 if wget "$DL_URL"; then
-  SHA1="$(sha1sum "$DL_FILE" | egrep -o '[a-f0-9]{40}')"
+  SHA1="$(sha1sum "$DL_FILE" | egrep -m 1 -o '[a-f0-9]{40}')"
   if [[ "$DL_SHA1" != "$SHA1" ]]; then
     echo "$DL_FILE $SHA1" >&2
     echo "Hashes do not match." >&2
