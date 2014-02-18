d73d7ec437e45b8e39cfc85c73afb9e0fbdcf0d6
Paul Malyschko <paulm@n0prisoners.com>
2014-02-18 22:24:54 +1030

    Add save object test

--- tests.py
+++ tests.py
@@ -1,11 +1,30 @@
+import os
 import unittest
+import parse
+
 
 class ParseTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        app_id = os.environ['APPLICATION_ID']
+        rest_api_key = os.environ['REST_API_KEY']
+        master_key = os.environ['MASTER_KEY']
+        parse.set_application('test-parse', app_id, rest_api_key, master_key)
 
     def tearDown(self):
-        pass
+        while True:
+            q = parse.Query('TestObject')
+            q.limit = 50
+            r = q.find()
+            if not r:
+                break
+            parse.Object.delete_all(r, ignore_acl=True)
+
+    def test_save_object(self):
+        obj = parse.Object('TestObject')
+        obj.save()
+        self.assertIsNotNone(obj.object_id)
+        self.assertIsNotNone(obj.created_at)
+
 
 if __name__ == '__main__':
     unittest.main()
