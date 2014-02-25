942ddfdbe25a5cc467157e2ae995166c337b3f18
Paul Malyschko <paulm@n0prisoners.com>
2014-02-25 15:09:53 +1030

    Add more print statements

--- tests.py
+++ tests.py
@@ -9,6 +9,7 @@ class ParseTestTimeout(Exception):
     pass
 
 def wait(r, timeout=30):
+    print ">>> wait..."
     end_time = time.time() + timeout
     while True:
         time.sleep(1)
@@ -19,12 +20,14 @@ def wait(r, timeout=30):
 
 class ParseTestCase(unittest.TestCase):
     def setUp(self):
+        print ">>> set up"
         app_id = os.environ['APPLICATION_ID']
         rest_api_key = os.environ['REST_API_KEY']
         master_key = os.environ['MASTER_KEY']
         parse.set_application('test-parse', app_id, rest_api_key, master_key)
 
     def tearDown(self):
+        print ">>> tear down"
         while True:
             q = parse.Query('TestObject')
             q.limit = 50
@@ -34,6 +37,7 @@ class ParseTestCase(unittest.TestCase):
             parse.Object.delete_all(r, ignore_acl=True)
 
     def test_save_object(self):
+        print ">>> test_save_object"
         obj = parse.Object('TestObject')
         obj.save()
         self.assertIsNotNone(obj.object_id)
