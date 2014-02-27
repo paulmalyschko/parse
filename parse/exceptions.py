2d4a0aae41362bd11ce8b5320ed9f8260385f419
Paul Malyschko <paulm@n0prisoners.com>
2014-02-27 17:12:13 +1030

    Fix super.__init__ in ParseException

--- parse/exceptions.py
+++ parse/exceptions.py
@@ -7,5 +7,6 @@ class ParseException(Exception):
     """Base Exception class"""
     
     def __init__(self, *args):
+        super(ParseException, self).__init__(*args)
         self.code = None
         self.reason = None
